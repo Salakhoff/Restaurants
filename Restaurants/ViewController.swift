ecf593c5409d84955a93cf0d369d0f24192d6888
Ilfat <salakhoff.ilfat@gmail.com>
2023-10-17 22:53:23 +0300

    Initial Commit

---
+++ Restaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurants
+//
+//  Created by Ильфат Салахов on 17.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
