9dd54ee11c6da407b7a0ada791528a74d7f1ad07
PavelDubinin <pavel.ios.develop@gmail.com>
2023-11-04 18:19:43 +0300

    Fix ext.

--- Restaurants/Extensions/UIStackView+ext.swift
+++ Restaurants/Extensions/UIStackView+ext.swift
@@ -9,18 +9,24 @@ import UIKit
 
 extension UIStackView {
     
-    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+    convenience init(
+        arrangedSubviews: [UIView],
+        axis: NSLayoutConstraint.Axis,
+        spacing: CGFloat
+    ) {
         self.init(arrangedSubviews: arrangedSubviews)
         self.axis = axis
         self.spacing = spacing
         self.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    convenience init(_ axis: NSLayoutConstraint.Axis,
-                               _ distribution:UIStackView.Distribution,
-                               _ alignment: UIStackView.Alignment,
-                               _ spacing: CGFloat,
-                               _ arrangedSubviews: [UIView] ) {
+    convenience init(
+        axis: NSLayoutConstraint.Axis,
+        distribution:UIStackView.Distribution,
+        alignment: UIStackView.Alignment,
+        spacing: CGFloat,
+        arrangedSubviews: [UIView]
+    ) {
             self.init(arrangedSubviews: arrangedSubviews)
             self.axis = axis
             self.distribution = distribution
